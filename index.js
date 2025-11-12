c8229537b3901d85a34ed7c7a170941e3bdd4e81
sure15 <gxishk15@gmail.com>
2025-11-13 02:21:02 +0800

    Finish password generator

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+  "/"];
 
 
+let generateButton = document.getElementById('generate-button')
+let rectOne = document.getElementById('rect-one')
+let rectTwo = document.getElementById('rect-two')
 
+function generatePassword() {
+  let password = ''
+  for (let i = 0; i < 15; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)]
+  }
+  return password
+}
 
+generateButton.addEventListener('click', function () {
+  rectOne.textContent = generatePassword()
+  rectTwo.textContent = generatePassword()
+})
